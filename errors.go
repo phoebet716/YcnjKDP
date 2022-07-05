5ee2f08aac15a2adc514af683f722c0eee243b13
phoebet716 <104357936+phoebet716@users.noreply.github.com>
2022-07-05 15:09:00 +0300

    Update errors.go

--- errors.go
+++ errors.go
@@ -5,12 +5,10 @@ var (
 	ErrBadRequest     = newError("Body invalid")
 	ErrForbidden      = newError("Forbidden")
 	ErrNotFound       = newError("Resource not found")
-	ErrRequestTimeout = newError("Timeout")
 )
 
 // HTTPError is custom HTTP error for API
 type HTTPError struct {
-	Message string `json:"message"`
 }
 
 func (e *HTTPError) Error() string {
